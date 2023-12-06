bb2b88545465810e2c59caf0c539da81c9c156c3
Qaleka <91965947+Qaleka@users.noreply.github.com>
2023-12-06 23:37:45 +0300

    123

---
+++ Code/script.js
@@ -0,0 +1,155 @@
+function submitForm() {
+    let form = document.getElementById('Form');
+
+    let n = parseInt(form.elements.n.value);
+    if (isNaN(n)) {//проверка N
+        alert("Неправильное значение для N");
+        return false
+    }
+    let k = parseInt(form.elements.k.value);
+    if (isNaN(k)) {//проверка k
+        alert("Неправильное значение для K");
+        return false
+    }
+
+    if (form.elements.m.value.length != k) {//проверка длины инф. вектора
+        alert("Длина информационного вектора должна быть равна N")
+        return false
+    }
+    let m = []
+    for (let char of form.elements.m.value) {//проверка значения инф. вектора
+        if (char !== '0' && char !== '1') {
+            alert("Неправильное значение для информационного вектора");
+            return false
+        }
+        m.push(+char)
+    }
+
+    if (form.elements.g.value.length > (n - k + 1)) {//проверка длины порождающего вектора
+        alert("Длина порождающего вектора должна быть равна N")
+        return false
+    }
+    let g = []
+    for (let char of form.elements.g.value) {//проверка порождающего вектора
+        if (char !== '0' && char !== '1') {
+            alert("Неправильное значение для порождающего вектора");
+            return false
+        }
+        g.push(+char)
+    }
+
+    console.log(n, k, m, g);
+    let coded = code(m, g, n, k);
+
+    let cyclic_code = document.getElementById("cyclic_code")
+    cyclic_code.textContent = "Циклический [" + n.toString() + ", " + k.toString() + "]-код: " + deleteZeroes(coded).join('')
+
+    let table = createTable()
+
+    for (let i = 1; i <= n; i++) {
+        let errors = 0
+        let detected = 0
+        generateErrors(n, i, (e) => {
+            errors += 1
+            if (checkError(coded, e, g)) {
+                detected += 1
+            }
+        })
+        let newRow = table.insertRow();
+
+        let cell1 = newRow.insertCell(0);
+        cell1.textContent = i.toString()
+        let cell2 = newRow.insertCell(1);
+        cell2.textContent = errors.toString()
+        let cell3 = newRow.insertCell(2);
+        cell3.textContent = detected.toString()
+        let cell4 = newRow.insertCell(3);
+        cell4.textContent = (detected / errors).toString()
+    }
+
+    return false;
+}
+
+function createTable() {
+    let table = document.getElementById('Table');
+    table.getElementsByTagName('thead')[0].innerHTML = '';
+    let t_body = table.getElementsByTagName('tbody')[0];
+    t_body.innerHTML = ''
+    let headerRow = document.getElementById('Table').insertRow();
+
+    const headerCellContents = [
+        "Кратность ошибки, i",
+        "Общее число ошибок, Cⁱₙ",
+        "Число обнаруженных ошибок, N₀",
+        "Обнаруживающая способность кода, C₀"
+    ];
+
+    headerCellContents.forEach(content => {
+        let th = document.createElement('th');
+        th.textContent = content;
+        headerRow.appendChild(th);
+    });
+
+    return t_body
+}
+
+function deleteZeroes(a) {
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] === 1) {
+            if (i !== 0) {
+                a = a.slice(i)
+            }
+            break
+        }
+    }
+    return a
+}
+
+function code(m, g, n, k) {
+    let tmp = m.slice().concat(Array(n - k).fill(0));
+    let reminder = divide(tmp, g)
+    return m.slice().concat(reminder)
+}
+
+function divide(a, b) {
+    for (let i = 0; i <= (a.length - b.length); i++) {
+        if (a[i] === 0) {
+            continue
+        }
+        for (let j = 0; j < b.length; j++) {
+            a[i + j] = a[i + j] ^ b[j]
+        }
+    }
+    return a.slice(-(b.length - 1))
+}
+
+function checkError(v, e, g) {
+    let r = Array.from(v)
+    for (let i = e.length; i > 0; i--) {
+        r[r.length - i] = r[r.length - i] ^ e[e.length - i]
+    }
+    let s = divide(r, g)
+    for (let i of s) {
+        if (i !== 0) {
+            return true
+        }
+    }
+    return false
+}
+
+function generateErrors(n, k, callback) {
+    function backtrack(current, onesCount, remainingZeros) {
+        if (onesCount + remainingZeros === 0) {
+            callback(current);
+            return;
+        }
+        if (onesCount > 0) {
+            backtrack(current.slice().concat([1]), onesCount - 1, remainingZeros);
+        }
+        if (remainingZeros > 0) {
+            backtrack(current.slice().concat([0]), onesCount, remainingZeros - 1);
+        }
+    }
+    backtrack([], k, n - k);
+    return;
+}
